95c2f0412ab76f22c93b5e902ef113b3d89932ec
Quincy Fer <quincy.e.fer@gmail.com>
2025-04-14 15:37:22 +0200

    Create Meals component. Fetch meals data from backend, output in ul

---
+++ src/components/Meals.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const Meals = () => {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+        // Handle error
+      }
+
+      const mealsData = await response.json();
+      setLoadedMeals(mealsData);
+    }
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Meals;
