c2664336c600b1f4de4de5fd8ad8167a208a839e
Quincy Fer <quincy.ferr@gmail.com>
2025-04-14 14:07:58 +0200

    2nd test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
       <h1>You got thiss 💪</h1>
       <p>Stuck? Not sure how to proceed?</p>
       <p>Don't worry - we've all been there. Let's build it together!</p>
+      <p>another test </p>
     </>
   );
 }
