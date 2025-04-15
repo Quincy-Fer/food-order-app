2a7c2bc65305e87ca7f967d0b9f098c23ed9cab9
Quincy Fer <quincy.e.fer@gmail.com>
2025-04-15 12:41:12 +0200

    Add counter to header component, amount of items in cart.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import logo from "../assets/logo.jpg";
 import Button from "./Button";
+import CartContext from "../store/CartContext";
+import { useContext } from "react";
 
 const Header = () => {
+  const cartCtx = useContext(CartContext);
+
+  const totalItems = cartCtx.items.reduce((totalNumber, item) => {
+    return totalNumber + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
@@ -9,7 +16,7 @@ const Header = () => {
         <h1>Foodie's Paradise</h1>
       </div>
       <nav>
-        <Button textOnly > Cart (0) </Button>
+        <Button textOnly> Cart ({totalItems}) </Button>
       </nav>
     </header>
   );
