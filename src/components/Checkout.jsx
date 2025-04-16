bf8b7d96fa5bdbdb7b7a61d07ecf3c32e4e5a998
Quincy Fer <quincy.e.fer@gmail.com>
2025-04-16 15:15:07 +0200

    Create Whole Checkout Form, links contexts to acces data and userprogress. Inside Cart.jsx add function to open Checkout Modal

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,20 +1,40 @@
 import { useContext } from "react";
 import Modal from "./Modal";
 import { currencyFormatter } from "../util/formatting";
+import Input from "./Input";
+import UserProgressContext from "../store/UserProgressContext";
+import CartContext from "../store/CartContext";
+import Button from "./Button";
 
 const Checkout = () => {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext)
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
+  
+  function handleClose(){
+    userProgressCtx.hideCheckout()
+  }
 
   return (
-    <Modal>
+    <Modal open={userProgressCtx.progress === "checkout"}>
       <form>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
+        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Email Address" type="email" id="email" />
+        <Input label="Street Address" type="text" id="street-address" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+        <p className="modal-actions">
+          <Button textOnly type="button" onClick={handleClose}>Close</Button>
+          <Button>Submit Order</Button>
+        </p>
       </form>
     </Modal>
   );
